4a028ab2254de644ca9cb197a9fc77e285621884
lily-vu <lilyvu22@gmail.com>
2023-03-21 20:23:21 -0500

    test: lily component

---
+++ components/inputs/TextInput.tsx
@@ -0,0 +1,13 @@
+
+export interface ITextInput {
+  sampleTextProp: string;
+}
+
+const TextInput: React.FC<ITextInput> = ({ sampleTextProp }) => {
+  return (
+
+    <input type="text" value={sampleTextProp}></input>
+  );
+};
+
+export default TextInput;
