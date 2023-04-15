8d7accddcd6ca734249df0761a26845d876ac8d0
AlexPepperoni <pronialex@gmail.com>
2023-04-15 12:28:39 -0500

    feat: added stories for the chart components

---
+++ pages/components/countryCharts.stories.tsx
@@ -0,0 +1,12 @@
+import { Meta, Story } from '@storybook/react';
+import Chart from '../components/countryCharts';
+
+export default {
+  component: Chart,
+} as Meta;
+
+export const Primary: Story = (args) => <Chart {...args} />;
+Primary.args = {
+  label: 'Button',
+  primary: true,
+};
